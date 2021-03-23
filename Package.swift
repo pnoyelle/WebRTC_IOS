3997cdb520a73e86c359595191a78b4fbe9401fc
pnoyelle <noyelle.pierre@gmail.com>
2021-03-23 16:53:02 +0100

    add check sum

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.3
 import PackageDescription
 let package = Package(
     name: "WebRTC_IOS",
@@ -14,7 +14,8 @@ let package = Package(
     targets: [
       .binaryTarget(
 	    name: "WebRTC",
-	    url: "https://github.com/pnoyelle/WebRTC_IOS/releases/download/4455/WebRTC.xcframework.zip"
+	    url: "https://github.com/pnoyelle/WebRTC_IOS/releases/download/4455/WebRTC.xcframework.zip",
+        checksum: "0c514486e7da15c85cd6e67c6e79848b8198b83ac66a96159f6494f0e8954ca9"
       )
     ]
 )
