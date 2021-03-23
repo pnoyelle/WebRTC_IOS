0c413607d019795cfade614cbd1732d0e3bf02d5
pnoyelle <noyelle.pierre@gmail.com>
2021-03-23 16:29:17 +0100

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.2
+import PackageDescription
+let package = Package(
+    name: "WebRTC_IOS",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "WebRTC_IOS",
+            targets: ["WebRTC"]),
+    ],
+    dependencies: [],
+    targets: [
+      .binaryTarget(
+	    name: "WebRTC",
+	    url: "https://github.com/pnoyelle/WebRTC_IOS/releases/download/4455/WebRTC.xcframework.zip"
+      )
+    ]
+)
